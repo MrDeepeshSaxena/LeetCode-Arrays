7fc994e8145663111874acc9b57703edfc1901e1
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-01-06 18:45:30 +0530

    1929_Build_Array_from_Permutation

---
+++ Build_Array_from_Permutation_1929.java
@@ -0,0 +1,19 @@
+public class Build_Array_from_Permutation_1929 {
+    public int[] buildArray(int[] nums) {
+
+         // Created an ans Array, whose length is equal to the length of nums Array
+        // i.e. no of elements in nums Array is equal to the no of elements in ans Array
+        int[] ans = new int[nums.length];
+
+        //Using a for loop to itterate over each element of nums and perform some operation on it
+        for(int i=0; i<nums.length; i++){
+
+            // Performed operation --> ans[i] = nums[nums[i]] on current element(ith element of nums Array)
+            // and then assigned it to the ith element of ans Array
+            ans[i] = nums[nums[i]];
+        }
+
+        //Returned the ans Array
+        return ans;
+    }
+}
