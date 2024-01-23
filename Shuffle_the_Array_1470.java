6427d8c41c8d7e10cb4febada08c038b0166f1a9
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-01-23 23:29:23 +0530

    1470_Shuffle_the_Array

---
+++ Shuffle_the_Array_1470.java
@@ -0,0 +1,11 @@
+public class Shuffle_the_Array_1470 {
+    public int[] shuffle(int[] nums, int n) {
+        int[] shuffled = new int[n * 2];
+        for (int i = 0; i < n; i++) {
+            shuffled[i * 2] = nums[i];
+            shuffled[i * 2 + 1] = nums[i + n];
+        }
+        return shuffled;
+    }
+
+}
