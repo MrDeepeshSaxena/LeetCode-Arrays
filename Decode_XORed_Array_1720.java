bdd52570ddb5cc1fd1dc6d043b1e024cded8ba48
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-01-28 22:58:58 +0530

    1720_Decode_XORed_Array

---
+++ Decode_XORed_Array_1720.java
@@ -0,0 +1,10 @@
+public class Decode_XORed_Array_1720 {
+    public int[] decode(int[] encoded, int first) {
+        int[] arr = new int[encoded.length + 1];
+        arr[0] = first;
+        for (int i = 0; i < encoded.length; i++) {
+            arr[i + 1] = encoded[i] ^ arr[i];
+        }
+        return arr;
+    }
+}
