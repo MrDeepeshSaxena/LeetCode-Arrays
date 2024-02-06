5b736e403f8fe948cb108465f89e44ca989dd1f7
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-02-06 23:29:33 +0530

    2828_Check_if_a_String_Is_an_Acronym_of_Words

---
+++ Check_if_a_String_Is_an_Acronym_of_Words_2828.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Check_if_a_String_Is_an_Acronym_of_Words_2828 {
+    public boolean isAcronym(List<String> words, String s) {
+        String acronym = "";
+        for (String word : words) {
+            acronym += word.charAt(0);
+        }
+        return acronym.equals(s);
+    }
+}
