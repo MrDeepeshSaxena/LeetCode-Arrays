c4873988e1fb97941569508724feb5c7de2206d9
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-02-13 23:36:50 +0530

    2960_Count_Tested_Devices_After_Test_Operations

---
+++ Count_Tested_Devices_After_Test_Operations_2960.java
@@ -0,0 +1,11 @@
+public class Count_Tested_Devices_After_Test_Operations_2960 {
+    public int countTestedDevices(int[] batteryPercentages) {
+        int ans = 0;
+
+        for (final int batteryPercentage : batteryPercentages)
+            if (batteryPercentage - ans > 0)
+                ++ans;
+
+        return ans;
+    }
+}
