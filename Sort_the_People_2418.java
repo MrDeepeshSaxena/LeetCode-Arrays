b1d4f4d15245133ae48b00f907484f8f13620d10
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-02-14 23:13:10 +0530

    2418_Sort_the_People

---
+++ Sort_the_People_2418.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Sort_the_People_2418 {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = heights.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            arr[i] = new int[] {heights[i], i};
+        }
+        Arrays.sort(arr, (a, b) -> b[0] - a[0]);
+        String[] ans = new String[n];
+        for (int i = 0; i < n; ++i) {
+            ans[i] = names[arr[i][1]];
+        }
+        return ans;
+    }
+}
