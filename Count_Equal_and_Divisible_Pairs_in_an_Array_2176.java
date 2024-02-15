a7c6c2b5abd6861b4379075e962d7c3d7addc641
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-02-15 23:50:58 +0530

    2176_Count_Equal_and_Divisible_Pairs_in_an_Array

---
+++ Count_Equal_and_Divisible_Pairs_in_an_Array_2176.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Count_Equal_and_Divisible_Pairs_in_an_Array_2176 {
+    public int countPairs(int[] nums, int k) {
+        int ans = 0;
+        Map<Integer, List<Integer>> numToIndices = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            numToIndices.putIfAbsent(nums[i], new ArrayList<>());
+            numToIndices.get(nums[i]).add(i);
+        }
+
+        for (List<Integer> indices : numToIndices.values()) {
+            Map<Integer, Integer> gcds = new HashMap<>();
+            for (final int i : indices) {
+                final int gcd_i = gcd(i, k);
+                for (final int gcd_j : gcds.keySet())
+                    if (gcd_i * gcd_j % k == 0)
+                        ans += gcds.get(gcd_j);
+                gcds.merge(gcd_i, 1, Integer::sum);
+            }
+        }
+
+        return ans;
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
