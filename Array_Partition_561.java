80d4ffcb3da3283962788943bde86742c079d50a
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-02-19 23:40:20 +0530

    561_Array_Partition

---
+++ Array_Partition_561.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Array_Partition_561 {
+    public int arrayPairSum(int[] nums) {
+        int ans = 0;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i += 2)
+            ans += nums[i];
+
+        return ans;
+    }
+}
