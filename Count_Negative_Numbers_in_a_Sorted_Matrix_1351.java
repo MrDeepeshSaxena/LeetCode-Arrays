f1482245c65201f739c57a3c7d3073a4faf814c4
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-02-23 22:43:31 +0530

    Count_Negative_Numbers_in_a_Sorted_Matrix_1351

---
+++ Count_Negative_Numbers_in_a_Sorted_Matrix_1351.java
@@ -0,0 +1,20 @@
+public class Count_Negative_Numbers_in_a_Sorted_Matrix_1351 {
+    public int countNegatives(int[][] grid) {
+        final int m = grid.length;
+        final int n = grid[0].length;
+        int ans = 0;
+        int i = m - 1;
+        int j = 0;
+
+        while (i >= 0 && j < n) {
+            if (grid[i][j] < 0) {
+                ans += n - j;
+                --i;
+            } else {
+                ++j;
+            }
+        }
+
+        return ans;
+    }
+}
