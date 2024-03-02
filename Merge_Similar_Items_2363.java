91f7e9b865e7181a17d494e805cfb1b9efd32b3f
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-03-02 23:37:24 +0530

    2363_Merge_Similar_Items

---
+++ Merge_Similar_Items_2363.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Merge_Similar_Items_2363 {
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        final int kMax = 1000;
+        List<List<Integer>> ans = new ArrayList<>();
+        int[] count = new int[kMax + 1];
+
+        for (int[] item : items1)
+            count[item[0]] += item[1];
+
+        for (int[] item : items2)
+            count[item[0]] += item[1];
+
+        for (int i = 1; i <= kMax; ++i)
+            if (count[i] > 0)
+                ans.add(Arrays.asList(i, count[i]));
+
+        return ans;
+    }
+}
