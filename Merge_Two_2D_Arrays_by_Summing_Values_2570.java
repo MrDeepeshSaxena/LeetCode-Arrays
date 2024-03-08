0f58862737d31d77757b66747ef4b62657c2d5f8
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-03-08 23:33:29 +0530

    2570_Merge_Two_2D_Arrays_by_Summing_Values

---
+++ Merge_Two_2D_Arrays_by_Summing_Values_2570.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Merge_Two_2D_Arrays_by_Summing_Values_2570 {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        final int kMax = 1000;
+        List<int[]> ans = new ArrayList<>();
+        int[] count = new int[kMax + 1];
+
+        addCount(nums1, count);
+        addCount(nums2, count);
+
+        for (int i = 1; i <= kMax; ++i)
+            if (count[i] > 0)
+                ans.add(new int[]{i, count[i]});
+
+        return ans.stream().toArray(int[][]::new);
+    }
+
+    private void addCount(int[][] nums, int[] count) {
+        for (int[] idAndVal : nums) {
+            final int id = idAndVal[0];
+            final int val = idAndVal[1];
+            count[id] += val;
+        }
+    }
+}
