3d09e70c1ae7abb6fe751c672676d07bebdf80b8
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-03-10 23:18:41 +0530

    136_Single_Number

---
+++ Single_Number_136.java
@@ -0,0 +1,10 @@
+public class Single_Number_136 {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+
+        for (final int num : nums)
+            ans ^= num;
+
+        return ans;
+    }
+}
