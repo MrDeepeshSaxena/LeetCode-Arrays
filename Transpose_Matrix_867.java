7dfd3ccda183c4cc012bb4f4d8b1dd86958c2258
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-03-24 23:28:37 +0530

    867_Transpose_Matrix

--- Transpose_Matrix_867.java
+++ Transpose_Matrix_867.java
@@ -5,7 +5,6 @@ public class Transpose_Matrix_867 {
         for (int i = 0; i < A.length; ++i)
             for (int j = 0; j < A[0].length; ++j)
                 ans[j][i] = A[i][j];
-
         return ans;
     }
 }
