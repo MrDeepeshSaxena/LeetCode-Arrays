8d2c164a988f1bdea663e44c2e24dae48e1df411
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-03-26 22:45:03 +0530

    2475_Number_of_Unequal_Triplets_in_Array

---
+++ Number_of_Unequal_Triplets_in_Array_2475.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Number_of_Unequal_Triplets_in_Array_2475 {
+    public int unequalTriplets(int[] nums) {
+        int ans = 0;
+        int prev = 0;
+        int next = nums.length;
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (final int num : nums)
+            count.merge(num, 1, Integer::sum);
+
+        for (final int freq : count.values()) {
+            next -= freq;
+            ans += prev * freq * next;
+            prev += freq;
+        }
+
+        return ans;
+    }
+}
