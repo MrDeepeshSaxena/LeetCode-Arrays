bcca2f0a50d25856e64ae86b6a877ee638e4d9a7
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-04-05 23:42:58 +0530

    908_Smallest_Range_I

---
+++ Smallest_Range_I_908.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Smallest_Range_I_908 {
+    public int smallestRangeI(int[] nums, int k) {
+        final int max = Arrays.stream(nums).max().getAsInt();
+        final int min = Arrays.stream(nums).min().getAsInt();
+        return Math.max(0, max - min - 2 * k);
+    }
+}
