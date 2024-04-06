0db754545fbdeb4fff7f1988d6b0a267b5541dca
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-04-06 23:45:55 +0530

    1502_Can_Make_Arithmetic_Progression_From_Sequence

---
+++ Can_Make_Arithmetic_Progression_From_Sequence_1502.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Can_Make_Arithmetic_Progression_From_Sequence_1502 {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int d = arr[1] - arr[0];
+        for (int i = 2; i < arr.length; ++i) {
+            if (arr[i] - arr[i - 1] != d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
