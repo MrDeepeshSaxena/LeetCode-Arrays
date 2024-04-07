e9689aeaf3d2de451e2d49fea0b38d7609906fb5
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-04-07 23:13:32 +0530

    2562_Find_the_Array_Concatenation_Value

---
+++ Find_the_Array_Concatenation_Value_2562.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Find_the_Array_Concatenation_Value_2562 {
+    public long findTheArrayConcVal(List<Integer> nums) {
+        long ans = 0;
+
+        for (int i = 0, j = nums.size() - 1; i <= j; ++i, --j) {
+            ans += nums.get(j);
+            if (i < j)
+                ans += nums.get(i) * Math.pow(10, (int) (Math.log10(nums.get(j))) + 1);
+        }
+
+        return ans;
+    }
+}
