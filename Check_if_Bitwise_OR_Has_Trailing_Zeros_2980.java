a27e568618cd4882c64dff30613427cf073e2cf8
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-04-09 23:24:06 +0530

    2980_Check_if_Bitwise_OR_Has_Trailing_Zeros

---
+++ Check_if_Bitwise_OR_Has_Trailing_Zeros_2980.java
@@ -0,0 +1,12 @@
+public class Check_if_Bitwise_OR_Has_Trailing_Zeros_2980 {
+    public boolean hasTrailingZeros(int[] nums) {
+        int countEven = 0;
+
+        for (final int num : nums)
+            if (num % 2 == 0)
+                ++countEven;
+
+        return countEven >= 2;
+
+    }
+}
