8f816ce862f89160de4e051c3f290d4cc9140ea3
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-04-13 20:25:25 +0530

    1200_Minimum_Absolute_Difference_

---
+++ Minimum_Absolute_Difference_1200.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Minimum_Absolute_Difference_1200 {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i + 1 < arr.length; ++i) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < min) {
+                min = diff;
+                ans.clear();
+            }
+            if (diff == min)
+                ans.add(Arrays.asList(arr[i], arr[i + 1]));
+        }
+
+        return ans;
+    }
+}
