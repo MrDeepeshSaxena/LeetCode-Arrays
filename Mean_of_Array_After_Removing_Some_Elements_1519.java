da5e1182f4a2d63360b2e6fb21459b26f21446d7
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-05-08 23:16:48 +0530

    1519_Mean_of_Array_After_Removing_Some_Elements

--- Mean_of_Array_After_Removing_Some_Elements_1519.java
+++ Mean_of_Array_After_Removing_Some_Elements_1519.java
@@ -5,6 +5,6 @@ public class Mean_of_Array_After_Removing_Some_Elements_1519 {
         Arrays.sort(arr);
         final int offset = arr.length / 20;
         return Arrays.stream(Arrays.copyOfRange(arr, offset, arr.length - offset)).average().orElse(0);
-
     }
+
 }
