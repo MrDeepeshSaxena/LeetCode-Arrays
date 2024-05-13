11d54a4a60603f9d92154ccd9828b3e0b159cd45
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-05-13 23:20:47 +0530

    1260_Shift_2D_Grid

---
+++ Shift_2D_Grid_1260.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Shift_2D_Grid_1260 {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        final int m = grid.length;
+        final int n = grid[0].length;
+        List<List<Integer>> ans = new ArrayList<>();
+        int[][] arr = new int[m][n];
+
+        k %= m * n;
+
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j) {
+                final int index = (i * n + j + k) % (m * n);
+                final int x = index / n;
+                final int y = index % n;
+                arr[x][y] = grid[i][j];
+            }
+
+        for (int[] row : arr)
+            ans.add(Arrays.stream(row).boxed().collect(Collectors.toList()));
+
+        return ans;
+
+    }
+}
